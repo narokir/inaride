5d8e6ae76f496f3ad66922eff55f05b863202bfd
Narek Kirakossian <narek.kirakossian@gmail.com>
2012-11-05 15:45:59 -0500

    update map markers

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,3 +4,28 @@
 //        event.preventDefault();
 //    });
 //});
+
+
+
+$(Document).ready(function(){
+    //Replace placeholder on inputs
+    $('#origin').blur(function() {
+        //alert('Handler for .focus() called.');
+        $(this).prop("placeholder","Origin")
+        //alert('Handler for .prop() called.');
+    });
+    $('#origin').focus(function() {
+        //alert('Handler for .focus() called.');
+        $(this).prop("placeholder","")
+    });
+    $('#destination').blur(function() {
+        //alert('Handler for .focus() called.');
+        $(this).prop("placeholder","Destination")
+        //alert('Handler for .prop() called.');
+    });
+    $('#destination').focus(function() {
+        //alert('Handler for .focus() called.');
+        $(this).prop("placeholder","")
+    });
+
+})
