ac372715c9da7391fc62e1ff9bd22d5839ebfe56
Narek Kirakossian <narek.kirakossian@gmail.com>
2012-11-10 17:23:48 -0500

    all fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-<<<<<<< HEAD
     redirect_to(request.env["HTTP_REFERER"])
-=======
-    redirect_to new_ride_url
->>>>>>> map-markers
   end
 
   def destroy
