30cd5b404261903c5058e067176d53416fb453d2
Narek Kirakossian <narek.kirakossian@gmail.com>
2012-12-05 04:40:06 -0500

    root :to

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Inoride::Application.routes.draw do
+  root to: 'static_pages#home'
   
   resources :maps
   resources :users
   resources :rides
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions
   
   get "maps/ride_route"
-  root :to => 'static_pages#home'
   
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
