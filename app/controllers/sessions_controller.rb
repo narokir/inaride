7016021fb5fb43a08c621cd6c126acf3f6f1abf4
Narek Kirakossian <narek.kirakossian@gmail.com>
2013-01-24 14:58:16 -0500

    stupid stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,18 +7,18 @@ class SessionsController < ApplicationController
   
   def create
     #store_location
-    user = User.find_by_email(params[:session][:email].downcase) || 
+    user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
       respond_to do |format|  
         format.html { redirect_back_or root_url }  
-        format.js # create.js 
+        format.js
       end
     else
       respond_to do |format|
 	format.html do
 	  flash.now[:error] = 'Invalid email/password combination'
-	  render action: "new"
+	  render 'new'
 	end
 	format.js do
 	  render :js => "$('<p>Invalid email password combination</p>').prependTo('#login-errors');"
@@ -26,6 +26,7 @@ class SessionsController < ApplicationController
       end
     end 
   end
+  
 
   def destroy
     session[:user_id] = nil
