06fa5fd0189de7706917d7160614bc4f1c6a6311
Narek Kirakossian <narek.kirakossian@gmail.com>
2013-01-24 23:15:02 -0500

    ajax search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,20 @@
 class SearchController < ApplicationController
-  
+  layout :choose_layout
   def search_rides
     if params[:origin].present? || params[:destination].present? || params[:date].present?
       @rides=Ride.search(params).limit(10)
-      render 'results'
+      respond_to do |format|
+	format.html { render 'results' }
+      end
     else
       render 'results'
       flash.now[:info] = "No params given"
     end
   end
+  
+  private  
+  def choose_layout  
+    (request.xhr?) ? 'special' : 'application'
+  end 
 
 end
