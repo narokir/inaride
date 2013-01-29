b02da02171b9444275cf4616964399a61c3b5859
Narek Kirakossian <narek.kirakossian@gmail.com>
2013-01-29 03:10:52 -0500

    turn on the fb popup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,29 +4,7 @@ class SessionsController < ApplicationController
   def new
     #@user = User.new
   end
-  
-#  def create
-#    user = User.find_by_email(params[:session][:email].downcase)
-#    if user && user.authenticate(params[:session][:password])
-#      sign_in user
-#      respond_to do |format|  
-#        format.html { redirect_back_or root_url }  
-#        format.js
-#      end
-#    else
-#      respond_to do |format|
-#	format.html do
-#	  flash.now[:error] = 'Invalid email/password combination'
-#	  render 'new'
-#	end
-#	format.js do
-#	  render :js => "$('<p>Invalid email password combination</p>').prependTo('#login-errors');"
-#	end
-#      end
-#    end 
-#  end
-  
-  
+    
   def create
     if
       request.env["omniauth.auth"]
@@ -40,18 +18,7 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url
   end
-  
-  def facebook_login
-    #raise request.env["omniauth.auth"].to_yaml
-    #raise request.env["omniauth.auth"].extra.raw_info.gender
-    @user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = @user.id
-      respond_to do |format|
-	format.html { redirect_back_or @user }
-	format.json { render :js => "alert('Facebook')" }
-      end
-  end
-  
+    
   private  
   def choose_layout  
     (request.xhr?) ? 'signin' : 'application'
@@ -60,7 +27,8 @@ class SessionsController < ApplicationController
   private
   def fbuser
     @user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = @user.id
+    sign_in @user
+    UserMailer.registration_email(@user).deliver
       respond_to do |format|
 	format.html { redirect_back_or @user }
 	format.json { render :js => "alert('Facebook')" }
