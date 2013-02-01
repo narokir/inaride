cd2e0b0e8336bfbe9d5c079fed2685f203d14731
Narek Kirakossian <narek.kirakossian@gmail.com>
2013-02-01 10:36:43 -0500

    refactor html to iclude masthead in body

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
   layout :choose_layout
   
+  def idex
+    @rides = Ride.paginate(:page => params[:page], :per_page => 10)
+  end
+  
   def search_rides
     if params[:origin].present? || params[:destination].present? || params[:date].present?
       @rides=Ride.search(params).limit(10)
