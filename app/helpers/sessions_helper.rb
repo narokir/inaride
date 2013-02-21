abab8e967107ea97f17b319ab790adb1c6b59719
Narek Kirakossian <narek.kirakossian@gmail.com>
2013-02-21 10:12:16 -0500

    mess up

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,5 +4,13 @@ module SessionsHelper
   #user_signed_in?
   #current_user
   #user_session
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end 
   
 end
